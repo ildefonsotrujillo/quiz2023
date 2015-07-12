067511232c88f698e3a76778dea838e8c9d0ab02
Ildefonso Trujillo <ildefonso.trujillo@gmail.com>
2015-07-12 18:09:44 +0100

    Creditos

--- routes/index.js
+++ routes/index.js
@@ -11,5 +11,9 @@ router.get('/', function(req, res) {
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
+/* GET Autores. */
+router.get('/author', function(req, res) {
+  res.render('author', { title: 'Quiz' });
+});
 
 module.exports = router;
